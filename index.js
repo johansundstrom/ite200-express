b2dd8feb204f3118d5d80221afe3506adfbe463c
johansundstrom <johan.sundstrom@mdh.se>
2021-11-28 18:12:01 +0100

    städat i index.js

--- index.js
+++ index.js
@@ -22,21 +22,16 @@ app.post('/api', (req, res) => {
   
 
   console.log(temp + " Celcius");
-  console.log(humid + "%");
-  //console.log(req.headers);
-  //res.json({"temp":data.temp});
+  console.log(humid + " %");
 
 
   //Sends a JSON response
   res.json({
-    //state: data.state,
-    //roundTrip: serverEpoch - data.clientEpoch + 'mS'
     temp: temp,
     humid: humid
   });
 });
 
-//Binds and listens for connections on the specified host and port
 app.listen(port, function() {
  console.log('Open: http://localhost:' + port);
 });
